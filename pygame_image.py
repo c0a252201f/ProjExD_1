a35fd5f5775c8b652ea92368a71f8aedf03a0331
masahiro <c0a252201f@edu.teu.ac.jp>
2026-06-09 16:14:34 +0900

    練習９こうかとん移動できる

--- pygame_image.py
+++ pygame_image.py
@@ -12,6 +12,9 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg1_img = pg.transform.flip(bg_img,True,False)
     kt_img = pg.image.load("fig/3.png")#こうかとん
+    kt_rct = kt_img.get_rect()#こうかとん移動
+    kt_rct.center = 300,200
+    screen.blit(kt_img,kt_rct)
     kt_img = pg.transform.flip(kt_img,True,False)
     tmr = 0
     while True:
@@ -19,11 +22,21 @@ def main():
 
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0,+1))
+        if key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((+1,0))
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg1_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kt_img, [300, 200])
+        screen.blit(kt_img, kt_rct)
         pg.display.update()
         tmr += 1       
         clock.tick(200)
