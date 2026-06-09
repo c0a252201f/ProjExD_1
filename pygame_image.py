b7dade57641a604fa666f677f8fab7600428e4a7
masahiro <c0a252201f@edu.teu.ac.jp>
2026-06-09 15:58:04 +0900

    練習７，８，９残像問題解決

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg1_img = pg.transform.flip(bg_img,True,False)
     kt_img = pg.image.load("fig/3.png")#こうかとん
     kt_img = pg.transform.flip(kt_img,True,False)
     tmr = 0
     while True:
+        
+
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg1_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kt_img, [300, 200])
         pg.display.update()
         tmr += 1       
